107044a7001dd28cc8f91b8ee4784f66f5ff1eeb
zealot09 <guol.zealot@gmail.com>
2015-03-18 00:24:21 +0800

    init social share

---
+++ public/javascripts/socialshare.js
@@ -0,0 +1,24 @@
+define(["jQuery"], function () {
+/**
+ *
+ * @param  share button config
+ *  config = {
+ *     hostImg : '',  shareBtns: [{ title: "", img: "", href: ""}]	
+ *  }
+ *
+ * @date     2015-03-18
+ * @author   QETHAN<qinbinyang@zuijiao.net>
+ */
+var SocialShare = function(config) {
+   this.config = config || {};
+   
+}
+//render the share buttons with circle
+SocialShare.prototype.circleRender = function(first_argument) {
+		
+};
+//render the share buttons with line
+SocialShare.prototype.lineRender = function () {
+		
+}
+});
