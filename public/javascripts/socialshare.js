ae0596251e5eec68629d10e1401e76d5709f06dc
zealot09 <guol.zealot@gmail.com>
2015-03-19 18:30:35 +0800

    css fix

--- public/javascripts/socialshare.js
+++ public/javascripts/socialshare.js
@@ -7,14 +7,14 @@ define(["./util"], function (util) {
  *  }
  *
  * @date     2015-03-18
- * @author   QETHAN<qinbinyang@zuijiao.net>
+ * @author   <guol.zealot@gmail.com>
  */
 var SocialShare = function(config) {
    this.config = config || {};
 
 }
 //render the share buttons with circle
-SocialShare.prototype.circleRender = function(first_argument) {
+SocialShare.prototype.circleRender = function() {
 		
 };
 //render the share buttons with line
@@ -23,12 +23,15 @@ SocialShare.prototype.lineRender = function () {
 }
 
 var initialize = function(config) {
-       var host = config.host || 'body';
+       var host = config.host || 'body', config = {};
        host = $(host);
+       config.element = host;
+       config.renderType = host.data('render') ? host.data('render'): "line";
+       config.hostImgSrc = host.data('src');
 
        var shareMainBtn = [
        		'<a class="smain disabled" title="click to show share panel" alt="click to show share panel">',
-       		'<img src="" alt="show share panel"/>',
+       		util.stringFormat('<img src="{0}" alt="show share panel"/>', config.hostImgSrc),
        		'</a>'
        ].join(''), 
        shareBtnTpl = util.stringFormat('<a class="sbutton {0}"> </a>', ""),
@@ -49,10 +52,10 @@ var initialize = function(config) {
        //build the social share tree declarative
        var shareBtnDom = '';
        _.each(shareBtnData, function(item) {
-       		shareBtnDom += util.stringFormat('<a class="sbutton {0}" href="{1}">{2}</a>', item.name, item.url, item.name);
-       		console.log(shareBtnDom);
+       		shareBtnDom += util.stringFormat('<a class="sbutton {0}" href="{1}" data-src="{2}">{2}</a>', item.name, item.url, item.src, item.name);
+       		//add the css rule dynamically
        });
-       //console.log(shareBtnDom);
+       
        var sBtnContainerDom = [
        			'<div class="scontainer default">',
        			shareBtnDom,
@@ -60,10 +63,19 @@ var initialize = function(config) {
        		].join('');
        //console.log(sBtnContainerDom);
        host.append($(shareMainBtn)).append($(sBtnContainerDom));
+       var sContainer = $('.scontainer', host), shareMainBtn = $('smain', host);;
+       //calculate the render absolute pixs & set the background
+       $('a.sbutton', sContainer).each(function(index) {
+       		var el = $(this);
+       		el.css({
+       			'background-image': util.stringFormat('url({0})', el.data('src')),
+       			left: (index + 1)* 20	
+       		});
+       });
 
        //attach the active & disable event using css3 transform
-}
 
+}
 return {
     initialize:  initialize
 }
