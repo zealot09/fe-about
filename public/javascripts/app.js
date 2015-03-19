4429a7d308147f864140b20033350f19ab77c3b5
lei.guo <lei.guo@reefoo.cn>
2015-03-19 17:24:53 +0800

    generate the dom tree

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,9 @@
 require.config({
     paths: {
     	"jQuery": "//s0.cdn.jituanyun.com/ajax/libs/jquery/1.8.3/jquery.min",
-    	"json": "//s0.cdn.jituanyun.com/ajax/libs/json3/3.2.5/json3.min"
+    	"json": "//s0.cdn.jituanyun.com/ajax/libs/json3/3.2.5/json3.min",
+    	"underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.2/underscore",
+    	"util": 'util'
     }
 });
 
--- public/javascripts/socialshare.js
+++ public/javascripts/socialshare.js
@@ -1,4 +1,4 @@
-define(["jQuery"], function () {
+define(["./util"], function (util) {
 /**
  *
  * @param  share button config
@@ -25,9 +25,43 @@ SocialShare.prototype.lineRender = function () {
 var initialize = function(config) {
        var host = config.host || 'body';
        host = $(host);
-       //add the host image and svg
-       host.append('')
-       host.append('')
+
+       var shareMainBtn = [
+       		'<a class="smain disabled" title="click to show share panel" alt="click to show share panel">',
+       		'<img src="" alt="show share panel"/>',
+       		'</a>'
+       ].join(''), 
+       shareBtnTpl = util.stringFormat('<a class="sbutton {0}"> </a>', ""),
+       dpReg = /^share/, dataConfig = host.data(), shareBtnData = [];
+
+       //roll the dataset format like: data-share-fb & data-fb-url as pairs to an object
+       for(var key in dataConfig) {
+       	if(dpReg.test(key)) {
+       		var surl = key.substring('share'.length, key.length).toLowerCase();
+       		shareBtnData.push({
+       			name: surl,
+       			src: dataConfig[key],
+       			url: dataConfig[surl + 'Url']
+       		});
+       	}	
+       }
+       console.log(JSON.stringify(shareBtnData));
+       //build the social share tree declarative
+       var shareBtnDom = '';
+       _.each(shareBtnData, function(item) {
+       		shareBtnDom += util.stringFormat('<a class="sbutton {0}" href="{1}">{2}</a>', item.name, item.url, item.name);
+       		console.log(shareBtnDom);
+       });
+       //console.log(shareBtnDom);
+       var sBtnContainerDom = [
+       			'<div class="scontainer default">',
+       			shareBtnDom,
+       			'</div>'	
+       		].join('');
+       //console.log(sBtnContainerDom);
+       host.append($(shareMainBtn)).append($(sBtnContainerDom));
+
+       //attach the active & disable event using css3 transform
 }
 
 return {
